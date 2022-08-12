20773ddd7b19a6d8c8db6fdf91eddee558d0ecd3
Weiboi <761788529@qq.com>
2022-08-12 20:43:05 +0800

    fix leak

--- src/main/java/com/hangout/util/Constants.java
+++ src/main/java/com/hangout/util/Constants.java
@@ -2,6 +2,6 @@ package com.hangout.util;
 
 public class Constants {
     public final static String USER_SESSION = "userSession";
-    public final static String APPID = "wxd0fc3ce95c6d5b80";
-    public final static String SECRET = "8abcf7103feceb4c5827f976987a9794";
+    public final static String APPID = "";
+    public final static String SECRET = "";
 }
